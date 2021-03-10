a45a89a40ad75c1ac7639ab9a03352d2447b4f1b
CoHutso <cole.hutson@okstate.edu>
2021-03-09 22:52:04 -0600

    Added Audio Clips and Animation for Airlock

---
+++ Assets/Cole/DoorAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimationController : MonoBehaviour
+{
+	public GameObject hand;
+	public void ShowHand()
+	{
+		hand.SetActive(true);
+	}
+
+	public void HideHand()
+	{
+		hand.SetActive(false);
+	}
+}
